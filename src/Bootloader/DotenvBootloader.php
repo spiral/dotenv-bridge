0e125050b80d9a4a1d694ee25e817367e685b5bf
spiralbot <ci-bot@spiralscout.com>
2022-09-13 06:58:22 +0000

    Merge pull request #786 from spiral/hotfix/router-subdomains

--- src/Bootloader/DotenvBootloader.php
+++ src/Bootloader/DotenvBootloader.php
@@ -1,37 +1,42 @@
 <?php
 
-/**
- * Spiral Framework.
- *
- * @license   MIT
- * @author    Anton Titov (Wolfy-J)
- */
-
 declare(strict_types=1);
 
 namespace Spiral\DotEnv\Bootloader;
 
 use Dotenv\Dotenv;
+use Spiral\Boot\AbstractKernel;
 use Spiral\Boot\Bootloader\Bootloader;
 use Spiral\Boot\DirectoriesInterface;
 use Spiral\Boot\EnvironmentInterface;
 
 final class DotenvBootloader extends Bootloader
 {
-    /**
-     * @param DirectoriesInterface $dirs
-     * @param EnvironmentInterface $env
-     */
-    public function boot(DirectoriesInterface $dirs, EnvironmentInterface $env)
-    {
-        $dotenvPath = $env->get('DOTENV_PATH', $dirs->get('root') . '.env');
+    private bool $init = false;
 
-        if (!file_exists($dotenvPath)) {
+    public function init(
+        AbstractKernel $kernel,
+        DirectoriesInterface $dirs
+    ): void {
+        $kernel->running(fn (EnvironmentInterface $env) => $this->loadEnvVariables($dirs, $env));
+    }
+
+    public function loadEnvVariables(DirectoriesInterface $dirs, EnvironmentInterface $env): void
+    {
+        if ($this->init) {
             return;
         }
 
-        $path = dirname($dotenvPath);
-        $file = basename($dotenvPath);
+        $this->init = true;
+
+        $dotenvPath = $env->get('DOTENV_PATH', $dirs->get('root') . '.env');
+
+        if (!\file_exists($dotenvPath)) {
+            return;
+        }
+
+        $path = \dirname($dotenvPath);
+        $file = \basename($dotenvPath);
 
         foreach (Dotenv::createImmutable($path, $file)->load() as $key => $value) {
             $env->set($key, $value);
